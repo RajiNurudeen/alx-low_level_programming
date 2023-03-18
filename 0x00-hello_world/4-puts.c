c0aa2e863a5add940e854960ce84c9dbbdd4131c
Raji Nurudeen <nurudeen486@gmail.com>
2023-03-18 14:25:43 +0000

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,10 @@
 #include <stdio.h>
+/**
+ * main - Entry point
+ * Description - A program that prints a line with puts function
+ * Return: 0 (Success)
+ */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
