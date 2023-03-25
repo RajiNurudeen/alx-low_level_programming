96e1fb130285064476c767257be65e48bde1ae2e
Raji Nurudeen <nurudeen486@gmail.com>
2023-03-25 19:10:55 +0000

    isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks for alphabetic character
+ *
+ * @c: takes in the value
+ * Return: Always (Success)
+ */
+
+int _isalpha(int c)
+{
+	if ((c >=65 && c <= 99) || (c >= 97 && c <= 122))
+	{
+	return (1);
+	}
+	else
+	{
+	return (0);
+	}
+}	
