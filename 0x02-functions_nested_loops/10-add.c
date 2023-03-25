4f6a5e9b609e96aaf5f02a88a5d59dff02e6cffb
Raji Nurudeen <nurudeen486@gmail.com>
2023-03-25 23:50:15 +0000

    a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - add two integers
+ * @x, y: the two integers to be added
+ *
+ * Return: Always (Success)
+ */
+
+int add(int x, int y)
+{
+	int res;
+
+	res = x + y;
+	return (res);
+}
