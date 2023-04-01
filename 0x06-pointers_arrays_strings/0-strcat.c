b6d6e1ba42871c1eaed88d6e3ef81c8e844ddf50
Raji Nurudeen <nurudeen486@gmail.com>
2023-04-01 12:11:12 +0000

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -5,6 +5,7 @@
  * @dest: first parameter
  * @src: second parameter
  * Return: Always (Success)
+ *
  */
 
 char *_strcat(char *dest, char *src)
@@ -14,8 +15,9 @@ char *_strcat(char *dest, char *src)
 	for (i = 0; dest[i] != '\0'; i++);
 
 	for (j = 0; src[j] != '\0'; j++, i++)
+	{
 		dest[i] = src[j];
-
+	}
 	dest[i] = '\0';
 
 	return (dest);
