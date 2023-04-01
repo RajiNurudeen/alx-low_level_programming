a9cc5b9c537b0741df77604d98b2180652286fa7
Raji Nurudeen <nurudeen486@gmail.com>
2023-04-01 00:26:00 +0000

    String copy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -8,15 +8,18 @@
  * Return: return a char
  */
 
-char *_strncpy(char *desc, char *src, int n)
+char *_strncpy(char *dest, char *src, int n)
 {
 	int i = 0, j = 0;
 
 	while (src[i++])
 		j++;
+
 	for (i = 0; src[i] && i < n; i++)
 		dest[i] = src[i];
+
 	for (i = j; i < n; i++)
 		dest[i] = '\0';
+
 	return (dest);
 }
