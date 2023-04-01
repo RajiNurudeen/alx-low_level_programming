f4bc1e8a6fff71a4cffad8739ea4b9dc6a2c527b
Raji Nurudeen <nurudeen486@gmail.com>
2023-04-01 14:26:37 +0000

    Always look up

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * string_toupper - Changes all lowercase to uppercase
+ * @str: string to change
+ * Return a character
+ */
+
+char *string_toupper(char *str)
+{
+	int i = 0;
+
+	while (str[i])
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] -= 32;
+		}
+		i++;
+	}
+	return (str);
+}
