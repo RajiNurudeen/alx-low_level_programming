8bb5f5a186b37274f1d9ae38919bd0e82ad6041d
Raji Nurudeen <nurudeen486@gmail.com>
2023-04-11 10:08:49 +0000

    Your subconsious is looking for the dreamer

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -2,7 +2,6 @@
 
 /**
  * _sqrt_actual - the fuction for the actual square root
- * _sqrt_recursion - natural square root of a number
  * @i: first paraameter of the actual sqrt
  * @n: the number to find it's natural root
  *
@@ -18,9 +17,15 @@ int _sqrt_actual(int n, int i)
 	if (i * i == n)
 	{
 		return (i);
-	}
+	
 	return (_sqrt_actual(n, i + 1));
 }
+
+/**
+ * _sqrt_recursion - ccalls the actual square root function
+ * @n: parameter that takes the value
+ * Return: the square root value
+ */
 
 int _sqrt_recursion(int n)
 {
