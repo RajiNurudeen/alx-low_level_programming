84566456cd20e224480e00bd15575d5999b45f5b
Raji Nurudeen <nurudeen486@gmail.com>
2023-04-30 04:04:36 +0000

    To be is to be the value of a variable

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -4,8 +4,10 @@
 
 /**
  * print_all - prints anything
- * @format: list of types of arguments passed to the function
+ * @format: list of arguments passed
  * @...: the printed strings
+ *
+ * Return: Success (0)
  */
 
 void print_all(const char * const format, ...)
