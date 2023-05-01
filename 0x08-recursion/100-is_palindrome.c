fd14788de581f2eedb9ddcba87cabd41a91c3124
Raji Nurudeen <nurudeen486@gmail.com>
2023-05-01 11:05:49 +0000

    Recursion

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -2,6 +2,7 @@
 
 int check_pal(char *s, int i, int len);
 int _strlen_recursion(char *s);
+
 /**
  * is_palindrome - checks if a string is a palindrome
  * @s: string to reverse
@@ -10,9 +11,9 @@ int _strlen_recursion(char *s);
  */
 int is_palindrome(char *s)
 {
-        if (*s == 0)
-                return (1);
-        return (check_pal(s, 0, _strlen_recursion(s)));
+	if (*s == 0)
+		return (1);
+	return (check_pal(s, 0, _strlen_recursion(s)));
 }
 /**
  * _strlen_recursion - returns the length of a string
@@ -22,9 +23,9 @@ int is_palindrome(char *s)
  */
 int _strlen_recursion(char *s)
 {
-        if (*s == '\0')
-                return (0);
-        return (1 + _strlen_recursion(s + 1));
+	if (*s == '\0')
+		return (0);
+	return (1 + _strlen_recursion(s + 1));
 }
 /**
  * check_pal - checks the characters recursively for palindrome
@@ -36,9 +37,9 @@ int _strlen_recursion(char *s)
  */
 int check_pal(char *s, int i, int len)
 {
-        if (*(s + i) != *(s + len - 1))
-                return (0);
-        if (i >= len)
-                return (1);
-        return (check_pal(s, i + 1, len - 1));
+	if (*(s + i) != *(s + len - 1))
+		return (0);
+	if (i >= len)
+		return (1);
+	return (check_pal(s, i + 1, len - 1));
 }
